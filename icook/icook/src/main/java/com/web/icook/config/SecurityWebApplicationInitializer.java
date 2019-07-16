909734752a93245764eb3c023a0dfa036a58b420
江慶庭 <bb3211367@gmail.com>
2019-07-16 13:57:44 +0800

    reset2
    2019.07.16

--- icook/icook/src/main/java/com/web/icook/config/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.web.icook.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
