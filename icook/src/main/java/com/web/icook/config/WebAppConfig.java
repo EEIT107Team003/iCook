c673fe2c3c71bd22df367bd4ddb0f1deaef52026
陳威翰 <herryfish2@gmail.com>
2019-07-18 15:45:20 +0800

    123

--- icook/src/main/java/com/web/icook/config/WebAppConfig.java
+++ icook/src/main/java/com/web/icook/config/WebAppConfig.java
@@ -11,7 +11,6 @@ import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
